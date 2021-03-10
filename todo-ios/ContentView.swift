3fab8b5238c63f65518fdc81260c484e376ab4cc
taisei sumiyama <r70s4effort@gmail.com>
2021-03-11 00:23:52 +0900

    Initial Commit

---
+++ todo-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-ios
+//
+//  Created by taisei sumiyama on 2021/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo-ios/todo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_iosApp.swift
+//  todo-ios
+//
+//  Created by taisei sumiyama on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct todo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
